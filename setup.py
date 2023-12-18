ba23d81da7246987bb3eec7d39f95e3a7bd2ca3e
Stuart <orangootan@gmail.com>
2023-12-18 15:53:16 -0800

    fix dist classfiiers

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: Linux',
+        'Operating System :: POSIX :: Linux',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'Framework :: AsyncIO',
