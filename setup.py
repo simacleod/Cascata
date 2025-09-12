83b74505570d1e52bce213d3621fc05050353a75
smacleod <orangootan@gmail.com>
2025-09-12 01:48:04 -0700

    bumpversion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cascata',
-    version='0.1.6',
+    version='0.1.7',
     author='Stuart MacLeod',
     author_email='orangootan@gmail.com',
     description='An asynchronous, multicore graph execution framework for Python.',
